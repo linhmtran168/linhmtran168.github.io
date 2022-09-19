d325704586cf4a9037ff6224960054db44fe02c3
Linh Tran <linh.mtran168@live.com>
2022-09-19 12:48:51 +0700

    test test

--- pages/about.tsx
+++ pages/about.tsx
@@ -4,7 +4,7 @@ export default function About(): JSX.Element {
   return (
     <Article article={{ title: 'About', uri: 'about' }}>
       <div className="mt-8">
-        <p>A lazy developer and a day dreamer. That’s all, folks :)</p>
+        <p>A lazy developer and a day dreamer. That’s all, folks!!! :)</p>
       </div>
     </Article>
   );
